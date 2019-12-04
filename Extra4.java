955cd65d6fbebbbb051747ae52a60347bef7dcee
Cesar-Sanchez-Herrera <57964628+Cesar-Sanchez-Herrera@users.noreply.github.com>
2019-12-03 18:52:36 -0600

    Add files via upload

---
+++ Extra4.java
@@ -0,0 +1,22 @@
+package com.softtek.java.academy.exercises;
+
+public class Extra4 {
+	
+	private String Hola;
+	
+	static void metodo() {
+		System.out.println("Este es un metodo");
+	}
+	
+	public static void main(String[]args) {
+		metodo();
+	}
+	
+	public Extra4() {
+		
+	}
+	
+	public interface instancia {
+		
+	}
+}
